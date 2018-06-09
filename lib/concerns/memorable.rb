0a7e548e85efadc6dc4f34bd156e7cf39a2b1884
jarretbryan <jarret.bryan@gmail.com>
2018-06-09 14:23:36 -0400

    solved basic tests

---
+++ lib/concerns/memorable.rb
@@ -0,0 +1,19 @@
+module Memorable
+
+  def reset_all
+    self.all.clear
+  end
+
+  def count
+    self.all.count
+  end
+
+end
+
+class Artist
+  extend Memorable
+end
+
+class Song
+  extend Memorable
+end
