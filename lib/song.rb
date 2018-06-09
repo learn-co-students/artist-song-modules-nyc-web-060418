235628c291802040a175896bf14adbcd9e2d8182
jarretbryan <jarret.bryan@gmail.com>
2018-06-09 14:56:55 -0400

    solved Paramble module

--- lib/song.rb
+++ lib/song.rb
@@ -3,6 +3,7 @@ require 'pry'
 class Song
   extend Memorable
   extend Findable
+  include Paramble
 
   attr_accessor :name
   attr_reader :artist
@@ -25,9 +26,9 @@ class Song
   #   @@songs.clear
   # end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+  # def to_param
+  #   name.downcase.gsub(' ', '-')
+  # end
 
   # def self.count
   #   self.all.count
